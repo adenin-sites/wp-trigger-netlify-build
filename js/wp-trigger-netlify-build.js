4549eb8331b179e10707f0a3193cf31511ff02fd
Charlotte Cameron <charlotte.cameron@adenin.com>
2022-02-21 10:15:49 +0000

    Update plugin for adenin WP instance

--- js/wp-trigger-netlify-build.js
+++ js/wp-trigger-netlify-build.js
@@ -1,11 +1,43 @@
 jQuery(document).ready(
-  (function ($) {
+  (function($) {
     // event handler for clicking the link button
-    $("#publish, #original_publish, .row-actions .trash, #delete-action").on("click", function (e) {
+    $("#wp-admin-bar-triggerStageBuild > a.ab-item").on("click", function(e) {
       $.ajax({
         type: "POST",
-        url: wpTriggerNetlifyBuildVars,
-        success: function (d) {
+        url: wpTriggerNetlifyBuildVars.stageurl,
+        success: function(d) {
+          console.log(d);
+        }
+      });
+    });
+    
+    $("#wp-admin-bar-triggerStageBuild-14 > a.ab-item").on("click", function(e) {
+      $.ajax({
+        type: "POST",
+        url: wpTriggerNetlifyBuildVars.stageurl,
+        data: JSON.stringify({'MORE_IMAGES': true}),
+        success: function(d) {
+          console.log(d);
+        }
+      });
+    });
+
+    $("#wp-admin-bar-triggerMasterBuild > a.ab-item").on("click", function(e) {
+      $.ajax({
+        type: "POST",
+        url: wpTriggerNetlifyBuildVars.masterurl,
+        success: function(d) {
+          console.log(d);
+        }
+      });
+    });
+    
+    $("#wp-admin-bar-triggerMasterBuild-14 > a.ab-item").on("click", function(e) {
+      $.ajax({
+        type: "POST",
+        url: wpTriggerNetlifyBuildVars.masterurl,
+        data: JSON.stringify({'MORE_IMAGES': true}),
+        success: function(d) {
           console.log(d);
         }
       });
